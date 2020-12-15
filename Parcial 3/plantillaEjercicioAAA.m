2bbf248df8a5533d115dc4b96710ddd51a41507a
juanresk <ja.restrepok@uniandes.edu.co>
2020-12-14 22:42:50 -0500

    saludes al David

--- Parcial 3/plantillaEjercicioAAA.m
+++ Parcial 3/plantillaEjercicioAAA.m
@@ -1,7 +1,7 @@
 clc, clear all, close all
 
 %% Inicio Graficar Nodos
-% Posiciones de los nodos---------------------------------------------------            
+% Posiciones de los nodos---------------------------------------------------
 nodesCoordinates=zeros(30,2);
 nodesCoordinates(1,1)=34; nodesCoordinates(1,2)=14; nodesCoordinates(2,1)=34; nodesCoordinates(2,2)=10;
 nodesCoordinates(3,1)=24; nodesCoordinates(3,2)=38; nodesCoordinates(4,1)=2; nodesCoordinates(4,2)=3;
@@ -23,139 +23,166 @@ RC = 10; % Radio de cobertura
 for i=1:length(nodesCoordinates)
     for j=1:length(nodesCoordinates)
         
-            dij=sqrt( ( nodesCoordinates(i,1)-nodesCoordinates(j,1) )^2+( nodesCoordinates(i,2)-nodesCoordinates(j,2) )^2);
-            if(dij<=RC) & i~=j
-                
-                matrLinks(i,j)=dij;
-                
-                line([nodesCoordinates(i,1), nodesCoordinates(j,1)], [nodesCoordinates(i,2), nodesCoordinates(j,2)],'LineStyle',':', 'Color','k', 'LineWidth',1);
-                hold on;
-            else
-                matrLinks(i,j)=inf;
-            end
+        dij=sqrt( ( nodesCoordinates(i,1)-nodesCoordinates(j,1) )^2+( nodesCoordinates(i,2)-nodesCoordinates(j,2) )^2);
+        if(dij<=RC) & i~=j
+            
+            matrLinks(i,j)=dij;
+            
+            line([nodesCoordinates(i,1), nodesCoordinates(j,1)], [nodesCoordinates(i,2), nodesCoordinates(j,2)],'LineStyle',':', 'Color','k', 'LineWidth',1);
+            hold on;
+        else
+            matrLinks(i,j)=inf;
+        end
         
     end
 end
 
 for i=1:length(nodesCoordinates)
-        x=nodesCoordinates(i,1);
-        y=nodesCoordinates(i,2);
-        plot(nodesCoordinates(i,1), nodesCoordinates(i,2), 'o', 'LineWidth',1,'MarkerEdgeColor','k', 'MarkerFaceColor','k', 'MarkerSize',7);
-        text(x+1, y+0.5, num2str(i), 'FontSize',8, 'FontWeight', 'bold', 'Color','k');
-        title('Red Celular')
-        % axis([0 110 0 60])
+    x=nodesCoordinates(i,1);
+    y=nodesCoordinates(i,2);
+    plot(nodesCoordinates(i,1), nodesCoordinates(i,2), 'o', 'LineWidth',1,'MarkerEdgeColor','k', 'MarkerFaceColor','k', 'MarkerSize',7);
+    text(x+1, y+0.5, num2str(i), 'FontSize',8, 'FontWeight', 'bold', 'Color','k');
+    title('Red Celular')
+    % axis([0 110 0 60])
 end
 
 %% Fin Graficar Nodos
 
 %% SIMULADOR DE EVENTOS DISCRETOS
 % Inicializacion de variables
-
+%Cantidad nodos
+cantidadNodos= length(nodesCoordinates);
 % Nodo fuente
-s= 9;
-
+inicio= 9;
 % Nodo destino
-d= 17;
+destino= 17;
 
-% Condicion de fin
-llegue = 0;
+% Centinela que me permite terminar la ejecucion de la simulacion
+parar = 0;
 
-% Vector repetidos
-vecinos_visitados =[];
+% Arreglo que contiene a los vecinos que ya se visitaron, estos despues se
+% pintan de azul o rojo dependiendo si se usaron o no
+ruta_vecinos =[];
 
 
 % Inicialización del tiempo de simulación
 t=0;
 
+%Arreglo que me sirve para agregar el padre de los nodos vecinos que se
+%visitan para luego mostrar la ruta que se tomo
+padre = [];
+actual = [];
+
+
+
 % Programacion del evento inicial o los eventos iniciales.
-evt.value = s;
-evt.t= t ; 
+evt.t= t ;
 evt.type='A';
+evt.value = inicio;
 
 % Adiciono el evento inicial o los eventos iniciales a la cola de eventos.
 evtQueue=evt;
-vecinos_visitados =[vecinos_visitados s];
-actual = [];
-predecesor = [];
-plot(nodesCoordinates(s,1), nodesCoordinates(s,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
+
+
 
 % Desarrollo de la simulacion
 while length(evtQueue)>0
-    evtAct=evtQueue(1); 
+    
+    evtAct=evtQueue(1);
     
     evtQueue(1)=[];
-    
     t=evtAct.t;
+    ruta_vecinos =[ruta_vecinos inicio];
+    
+    %Aqui se esta pintando el nodo inicio
+    plot(nodesCoordinates(inicio,1), nodesCoordinates(inicio,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
     
     % Procesamiento del evento A
-    if evtAct.type=='A' 
+    if evtAct.type=='A'
         
-       % fprintf('Valor : %f , esta visitado %s \n',evtAct.value ,string(ismember(evtAct.value, vecinos_visitados)))
-        
-        for j =1:length(nodesCoordinates)
-
+        for j =1:cantidadNodos
             
-            if matrLinks(evtAct.value,j) ~= inf
+            
+            if matrLinks(evtAct.value,j) < inf
                 
-                %fprintf('Vecino de %f es %f \n', evtAct.value, j) 
-                if j == d
+                %Si ya llegué al destino entonces se debe detener la
+                %simulación y se debe pintar el camino recorrido.
+                if j == destino
+                    parar =1;
                     actual = [actual j];
-                    predecesor = [predecesor evtAct.value];
-                    llegue =1;
-               
-                elseif ~ismember(j, vecinos_visitados) && llegue ==0
-                     
-                       
+                    padre = [padre evtAct.value];
+                elseif parar ==0 && ~any( arrayfun( @(x) isequal( j, x ), ruta_vecinos ) ) 
+                    %for z=1:length(vecinos_visitados)
+                    %if vecinos_visitados(z)~=j
                     newEvt.value = j;
-                    %fprintf('agrega %f \n',j)
                     actual = [actual j];
-                    predecesor = [predecesor evtAct.value];
-                    newEvt.t=t+ matrLinks(evtAct.value,j)*3.333; 
-                    newEvt.type='A'; 
-                    evtQueue=[evtQueue newEvt];       
-                    vecinos_visitados =[vecinos_visitados j ];
+                    padre = [padre evtAct.value];
+                    
+                    %Aquí se le añade el tiempo de enviar un paquete desde
+                    %un nodo a otro.
+                    
+                    newEvt.t=t+ matrLinks(evtAct.value,j)*3.333;
+                    
+                    %Se crea un nuevo evento tipo A y este recorrerá sus
+                    %vecinos y sus vecinos recorrerán sus vecinos y así
+                    %sucesivamente hasta encontrar el nodo destino
+                    newEvt.type='A';
+                    evtQueue=[evtQueue newEvt];
+                    ruta_vecinos =[ruta_vecinos j ];
+                    %Se pintan los vecinos de azul. Mas tarde se pintaran
+                    %de rojo los que en realidad se utilicen.
                     plot(nodesCoordinates(j,1), nodesCoordinates(j,2), 'o', 'LineWidth',1,'MarkerEdgeColor','b', 'MarkerFaceColor','b', 'MarkerSize',7);
-
-                 end
+                end
             end
         end
-        
-    end          
-
-    if llegue ==1
+    end
+    
+    
+    if parar ==1
         break;
     end
-       
+    
     % Organizacion de la cola de eventos
     flag=1;
     while flag==1
-        flag=0; 
+        flag=0;
         for i=1:(length(evtQueue)-1)
             if evtQueue(i).t>evtQueue(i+1).t
                 temp=evtQueue(i);
                 evtQueue(i)=evtQueue(i+1);
                 evtQueue(i+1)=temp;
-                flag=1; 
+                flag=1;
             end
         end
     end
     % Mostrado de la cola de eventos:
-    %fprintf('\nCola de eventos:\n');
-    %for i=1:length(cola)
-     %   fprintf('Evento %s \n',cola(i));
-    %end
-    %fprintf('----------------\n');
+    fprintf('\nCola de eventos:\n');
+    for i=1:length(evtQueue)
+        fprintf('Evento %s en t=%f\n',evtQueue(i).type,evtQueue(i).t);
+    end
+    fprintf('----------------\n');
     % pause;
 end
 
-M = containers.Map(actual ,predecesor);
-pintarNodo = d;
-while pintarNodo ~= s
-     plot(nodesCoordinates(pintarNodo,1), nodesCoordinates(pintarNodo,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
-     pintarNodo = M(pintarNodo); 
+fprintf('Tiempo transcurrido: %f  \n',t);
+
+
+
+
+M = containers.Map(actual ,padre);
+pintarNodo = destino;
+%for z=1:length(actual)
+%    plot(nodesCoordinates(pintarNodo,1), nodesCoordinates(pintarNodo,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
+%    pintarNodo=padre(z);
+%end
+
+
+
+while pintarNodo ~= inicio
+    plot(nodesCoordinates(pintarNodo,1), nodesCoordinates(pintarNodo,2), 'o', 'LineWidth',1,'MarkerEdgeColor','r', 'MarkerFaceColor','r', 'MarkerSize',7);
+    pintarNodo = M(pintarNodo);
 end
 
-fprintf('El tiempo de simulación que tomó ir desde el nodo origen hasta el nodo destino fue: %f  \n',t);
 
 
 
